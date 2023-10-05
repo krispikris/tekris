0b705d23ca064ecadd876b6e60243d0448b989b7
Kris <kris@kristopherhan.com>
2023-10-05 15:04:43 -0700

    npm setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './styles.css';
-import Game from '/src/components/Game';
+import Game from './components/Game';
 
 export default function App() {
     return (
         <div className='App'>
             <Game rows={20} columns={10} />
-            <h1>Hello Tekris</h1>
+            <h1>Peace Tekris</h1>
             <h2>Start Editing to see some magic happen!</h2>
         </div>
     )
