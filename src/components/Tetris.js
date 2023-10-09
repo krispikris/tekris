62d45227d085e4b60d55a67659ac5298644f5550
Kris <kris@kristopherhan.com>
2023-10-09 13:05:33 -0700

    updates before moving to previews

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,17 +1,20 @@
 import './Tetris.css';
 import Board from '../components/Board';
 import GameStats from '../components/GameStats';
+import Previews from '../components/Previews';
 import {useBoard} from '../hooks/useBoard';
 import {useGameStats} from '../hooks/useGameStats';
 
 const Tetris = ({rows, columns, setGameOver}) => {
     const [gameStats, addLinesCleared] = useGameStats();
     const [board, setBoard] = useBoard({rows, columns});
-    
+    const player = {tetrominoes: []};
+
     return (
         <div className='Tetris'>
-            <Board board={board}/>
+            <Board board={board} />
             <GameStats gameStats={gameStats} />
+            <Previews tetrominoes={player.tetrominoes} />
         </div>
     );
 };
