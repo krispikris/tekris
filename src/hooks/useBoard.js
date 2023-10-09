e0f3148617c6b2cffe3441c8980d28ab3e90290c
Kris <kris@kristopherhan.com>
2023-10-09 15:32:11 -0700

    creating useBoard

--- src/hooks/useBoard.js
+++ src/hooks/useBoard.js
@@ -1,7 +1,12 @@
 import { useState } from 'react';
 import { buildBoard } from '../utils/Board';
 
-export const useBoard = ({rows, columns}) => {
+export const useBoard = ({rows, 
+    columns, 
+    player, 
+    resetPlayer, 
+    addLinesCleared
+}) => {
     const [board] = useState(buildBoard({rows, columns}));
 
     return [board];
