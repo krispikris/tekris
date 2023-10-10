de04441461b0fac5774d8c9fdc834ca6cec4b005
Kris <kris@kristopherhan.com>
2023-10-10 11:30:32 -0700

    GameController started

---
+++ src/components/GameController.js
@@ -0,0 +1,30 @@
+// import React from 'react';
+import './GameController.css';
+
+const GameController = ({
+    board, 
+    gameStats, 
+    player, 
+    setGameOver, 
+    setPlayer
+}) => {
+    const onKeyUp = ({code}) => {
+        console.log(`onKeyUp: ${code}`)
+    }
+
+    const onKeyDown = ({code}) => {
+        console.log(`onKeyDown: ${code}`)
+    }
+
+    return (
+        <input 
+            className='GameController'
+            type='text'
+            onKeyDown={onKeyDown}
+            onKeyUp={onKeyUp}
+            autoFocus
+        />
+    );
+}
+        
+export default GameController;
