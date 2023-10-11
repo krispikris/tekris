6521f9378138e6bbfefde30ff80f842b70c75093
Kris <kris@kristopherhan.com>
2023-10-10 20:19:38 -0700

    movement within board details

--- src/utils/Board.js
+++ src/utils/Board.js
@@ -35,3 +35,20 @@ export const nextBoard = ({board, player, resetPlayer, addLinesCleared}) => {
         size: {...board.size}
     }
 };
+
+export const isWithinBoard = ({board, position, shape}) => {
+    for (let y = 0; y < shape.length; y++) {
+        const row = y + position.row;
+
+        for (let x = 0; x < shape[y].length; x++) {
+            if (shape[y][x]) {
+                const column = x + position.column;
+                const isValidPosition = board.rows[row] && board.rows[row][column];
+
+                if (!isValidPosition) return false;
+            }
+        }
+    }
+
+    return true;
+}
