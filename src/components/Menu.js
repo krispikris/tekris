7f4018e04ea79cd439739bcd39a68cdb63c2bf23
Kris <kris@kristopherhan.com>
2023-10-31 19:16:58 -0700

    update

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,7 +4,7 @@ const Menu = ({onClick}) =>
     <div className='Menu'>
         <button className='Button' onClick={onClick}>
             {/* Play Te[k]ris! */}
-            PLAY TEKRIS!
+            PLAY TE[K]RIS!
         </button>
     </div>
 
